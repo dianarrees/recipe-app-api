c26e6947f1bdc3246d75663ae9a5133ab7472d3f
zksystems <zksystems@zksystemss-mbp.speedport.ip>
2022-10-20 19:30:53 +0200

    fixed typos

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,5 +1,5 @@
 """
-Sample test
+Sample tests
 """
 
 from django.test import SimpleTestCase
@@ -7,17 +7,17 @@ from django.test import SimpleTestCase
 from app import calc
 
 
-class CalcTest(SimpleTestCase):
-    """test the calc module"""
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
 
     def test_add_numbers(self):
-        """Test adding numbers together"""
+        """Test adding numbers together."""
         result = calc.add(5, 6)
 
         self.assertEqual(result, 11)
 
     def test_subtract_numbers(self):
-        """Test adding numbers together"""
+        """Test subtracting numbers."""
         result = calc.subtract(10, 15)
 
         self.assertEqual(result, 5)
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,10 +10,10 @@ class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful"""
         email = 'test@example.com'
-        password = 'testpass123Z'
+        password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password,
+            email=email,
+            password=password,
         )
 
         self.assertEqual(user.email, email)
@@ -21,7 +21,7 @@ class ModelTests(TestCase):
 
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users"""
-        sample_emails= [
+        sample_emails = [
             ['test1@EXAMPLE.com', 'test1@example.com'],
             ['Test2@Example.com', 'Test2@example.com'],
             ['TEST3@example.COM', 'TEST3@example.com'],
